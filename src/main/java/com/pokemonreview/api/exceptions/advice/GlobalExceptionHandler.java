e482b22165aeefd6b2736ead8afb56e55df1c2fe
mysoyul <vega2k_@naver.com>
2024-04-16 16:21:10 +0900

    cors config add

--- src/main/java/com/pokemonreview/api/exceptions/advice/GlobalExceptionHandler.java
+++ src/main/java/com/pokemonreview/api/exceptions/advice/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,7 +30,7 @@ public class GlobalExceptionHandler {
     }
 
     /*
-        Spring6버전에 추가된 ProblemDetail 객체에 에러정보를 담아서 리턴하는 방법 
+        Spring6버전에 추가된 ProblemDetail 객체에 에러정보를 담아서 리턴하는 방법
      */
     protected ProblemDetail handleException(MyResourceException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatus(e.getStatusCode());
@@ -49,4 +52,13 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatusCode.valueOf(500));
     }
 
+    @ExceptionHandler(value = AccessDeniedException.class)
+    public void accessDeniedExceptionHandler(Exception e) {
+        throw new AccessDeniedException(e.getMessage());
+    }
+
+    @ExceptionHandler(value = BadCredentialsException.class)
+    public void badCredentialExceptionHandler(BadCredentialsException e){
+        throw new BadCredentialsException(e.getMessage());
+    }
 }
